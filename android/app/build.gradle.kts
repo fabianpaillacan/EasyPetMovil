73bd7713d2c310e99df36a9b40337d71cb1adb13
fabianpaillacan <fabianpaillacanh@gmail.com>
2025-08-29 22:57:31 -0400

    Actualiza configuración de build para compatibilidad con plugins de Flutter en Android.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.easypet"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
